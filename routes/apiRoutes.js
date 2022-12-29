441f6575e86bdc47ae17fb047eda7e33e069f6aa
Christin Carter <raspberry.muppet@gmail.com>
2022-12-29 16:54:35 -0700

    added GET route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,20 @@
 const db = require("../db/db.json");
 const fs = require("fs");
+const router = require('express').Router();
 
-module.exports = function (app) {
-    app.post("/api/notes", function (req, res) {
-        
-    }
-}
+router.get('/notes', function (req, res) {
+    store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
+});
+    
+router.post('/notes', function (req, res) {
+    
+});
+
+router.delete('/notes', function (req, res){
+
+});
+    
+module.exports = router;
