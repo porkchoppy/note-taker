535fdfe9efca015bc650893a31386c68bbf4bf7b
Christin Carter <raspberry.muppet@gmail.com>
2023-01-09 16:37:14 -0700

    working on get/post/delete routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,30 @@
 const db = require("../db/db.json");
 const fs = require("fs");
 const router = require('express').Router();
-const uniqid = require("uniqid");
+const { v4: uuidv4 } = require('uuid');
 
-router.get('/notes', (req, res) => {
-       res.json(db);
+router.get("/notes", (req, res) => {
+  res.json(db);
 });
     
 router.post('/api/notes', (req, res) => {
-       const myNewNote = req.body;
-       myNewNote.id = uniqid(myNewNote.id);
+  const myNewNote = req.body;
+    myNewNote.id = uuidv4(myNewNote.id);
 
-       db.push(myNewNote);
+  db.push(myNewNote);
 
-       fs.writeFile("db/db.json", JSON.stringify(db), (err) => {
-         if (err) {
-           return res.sendStatus(500);
-         }
-         res.json(myNewNote);
-       });
+  fs.writeFile("db/db.json", JSON.stringify(db), (err) => {
+    if (err) {
+      return res.sendStatus(500);
+    }
+      res.json(myNewNote);
+  });
 });
 
 router.delete('/notes/:id', (req, res) => {
-       
+  const params = req.params.id
+  updateDb(params, notes);
+  res.redirect("");
 });
     
 module.exports = router;
